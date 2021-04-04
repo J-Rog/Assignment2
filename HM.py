40b21a338c428391eb1da1ed28a464121ffe30b6
Jonathan Rogers <jonathanrogers@protonmail.com>
2021-04-04 05:37:58 -0500

    It shouldn't be this hard to remember, so why is it?

--- HM.py
+++ HM.py
@@ -10,11 +10,23 @@ lines = raw_corpus['PlayerLine'].to_numpy()
 
 lineOlines = []
 
-#Remove all punctuation
 for i in range(0,len(lines)):
-    lines[i] = re.sub(r'[^\w\s]', '', lines[i])
-
+    #Remove all punctuation and uppercase letters
+    lines[i] = re.sub(r'[^\w\s]', '', lines[i]).lower()
+    #Turn each sententce into a list
     target = lines[i].split()
 
+    #Create a 1d array of all lines
     for j in target:
         lineOlines.append(j)
+
+word_count = {}
+
+#Gets each word count and a 1d object of every word
+for i in lineOlines:
+    if i in word_count:
+        word_count[i] = word_count[i] + 1
+    else:
+        word_count[i]=1
+
+x = np.array(word_count.keys())
