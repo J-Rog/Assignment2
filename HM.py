08072fbb6ebef3557b8eeff62f67a9fa434c7d4c
Jonathan Rogers <jonathanrogers@protonmail.com>
2021-04-04 05:08:27 -0500

    Progress is slow and sleep while a boon to my overall mental state eludes me. I realize my commit messages are longer than my code base. This realization has hurt already dwindling morale and made me sympathise with Sisyphus.

--- HM.py
+++ HM.py
@@ -8,6 +8,13 @@ raw_corpus = pd.read_csv('Shakespeare_data.csv')
 #Isolate the lines and convert them into a numpy array.
 lines = raw_corpus['PlayerLine'].to_numpy()
 
+lineOlines = []
+
 #Remove all punctuation
 for i in range(0,len(lines)):
     lines[i] = re.sub(r'[^\w\s]', '', lines[i])
+
+    target = lines[i].split()
+
+    for j in target:
+        lineOlines.append(j)
