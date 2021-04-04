4f3ca8d10eb8bcef5b8ae92a849fe5388b62f806
Jonathan Rogers <jonathanrogers@protonmail.com>
2021-04-04 02:59:51 -0500

    I have learned now to save often for if I repeat the err that lead me to redoing this assignment I fear I shall never complete it out of shear apathy. I do not like nor hate my position but I am overwhelmingly indifferent towards it, which in many ways is worse. Anger is at least something.

--- HM.py
+++ HM.py
@@ -1,9 +1,13 @@
 import pandas as pd
+import numpy as np
+import re
 
-#Download the file
+#Open the file
 raw_corpus = pd.read_csv('Shakespeare_data.csv')
 
-#Isolate the lines
-lines = raw_corpus['PlayerLine']
+#Isolate the lines and convert them into a numpy array.
+lines = raw_corpus['PlayerLine'].to_numpy()
 
-print(lines.head())
+#Remove all punctuation
+for i in range(0,len(lines)):
+    lines[i] = re.sub(r'[^\w\s]', '', lines[i])
