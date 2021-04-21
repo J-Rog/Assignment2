6aaed603cbdde8807a19e0f21b335ca85a5c06f9
Jonathan Rogers <jonathanrogers@protonmail.com>
2021-04-21 02:39:15 -0500

    Why is it that motivation only exists at ridiculous hours? My mind feels cleared now after ~18 hours of conciousness than even after a good night's sleep. Tonight or perhaps this morning this project will be recompleted.

--- HM.py
+++ HM.py
@@ -34,25 +34,23 @@ for i in lineOlines:
 #Now I have every word mapped to a unique index
 x = np.array(list(word_count.keys()))
 
-#Create a blank transition table of size 27381 x 27381
-transition = np.zeros((len(x),len(x)))
-
-print("Transition shape is " + str(np.shape(transition)))
+#Create a blank emission table of size 27381 x 27381
+emission = np.zeros((len(x),len(x)))
 
 startTime = datetime.now()
 
-print("Begin raw transition table " + str(startTime))
+print("Begin raw emission table " + str(startTime))
 
-#Populate the transition table with raw counts
+#Populate the emission table with raw counts
 for i in range(1,len(lineOlines)):
 
     current_index = np.where(x == lineOlines[i])[0][0]
 
     past_index = np.where(x == lineOlines[i-1])[0][0]
 
-    transition[past_index][current_index] += 1
+    emission[past_index][current_index] += 1
 
-print("End raw transition table " + str(datetime.now() - startTime))
+print("End raw emission table " + str(datetime.now() - startTime))
 
 
 #Convert raw data into probabilities
@@ -61,15 +59,13 @@ print("Begin probability table " + str(startTime))
 
 for i in range(0,len(x)):
 
-    row_total = sum(transition[i])
+    row_total = sum(emission[i])
 
     for j in range(0,len(x)):
 
-        transition[i][j] = transition[i][j]/row_total
+        emission[i][j] = emission[i][j]/row_total
 
 print("End probability table " + str(datetime.now() - startTime))
-
-
 
 choice = 0
 while(True):
@@ -95,15 +91,14 @@ while(True):
                 queary = np.where(x == word[0])[0][0]
             except Exception:
                 pass
-            print(queary)
-            word[0] = x[np.argmax(transition[queary])]
+            word[0] = x[np.argmax(emission[queary])]
 
             #Penalize duplicates
             if word[0] in word_list:
-                temp = transition[queary][np.argmax(transition[queary])]
-                transition[queary][np.argmax(transition[queary])] = 0
-                word[0] = x[np.argmax(transition[queary])]
-                transition[queary][np.argmax(transition[queary])] = temp
+                temp = emission[queary][np.argmax(emission[queary])]
+                emission[queary][np.argmax(emission[queary])] = 0
+                word[0] = x[np.argmax(emission[queary])]
+                emission[queary][np.argmax(emission[queary])] = temp
 
             word_list.append(word[0])
 
@@ -116,7 +111,82 @@ while(True):
 
 
     if choice == 2:
-        print("Choice 2: Predict Text")
+
+        #Make a transition table
+        #           |Novel| Previous | Degree2 |
+        #Novel      |_.3__|___.45____|__.5_____|
+        #Previous   |_.4__|___.5_____|__.55____|
+        #Degree2    |_.35_|___.6_____|__.05____|
+        #Assumption the next word is based only on previous word counts up to 2
+        transition = np.array([[0.3,0.45,0.5],[0.4,0.5,0.55],[0.5,0.55,0.05]])
+
+        #Get user input
+        corpus = input("Please some text: ").split()
+
+        #Initialize probability with any given word assuming even probability
+        emmision_probability = 1/len(x)
+
+        #Three states novel, previous, 2Degree (0,1,2) respectively
+        state = 0
+        previous_state = 0
+
+        #Tracks probability
+        probability_sum = 0
+
+        novel = []
+        previous = []
+        degree2 = []
+
+
+
+        for i in range(1, len(corpus)):
+
+            try:
+                queary = np.where(x == corpus[i-1])[0][0]
+            except Exception:
+                pass
+
+            if corpus[i] in novel:
+                state = 0; #Novel
+                novel.remove(corpus[i])
+                previous.append(corpus[i])
+
+            elif corpus[i] in previous:
+                state = 1 #Previous
+                previous.remove(corpus[i])
+                degree2.append(corpus[i])
+
+            elif corpus[i] in degree2:
+                state = 2 #Degree2
+
+            elif corpus[i] in x:
+                state = 0 #Novel
+                novel.append(word)
+
+            else: #corpus[i] not in Shakespeare_data
+                state = 0 #Novel
+
+
+            if state == 0:
+                emmision_probability = 1/len(x)
+            else:
+                emmision_probability = [emission[queary][np.argmax(emission[queary])]]
+
+
+            probability_sum = probability_sum + emmision_probability * \
+                              transition[previous_state][state]
+
+            previous_state = state
+
+        #Corpus probability found
+
+        prediction_num = int(input("How many words would you like to predict"))
+
+        for i in range(0, prediction_num):
+
+
+
+
     if choice == 3:
         print("Choice 3: Exit")
         break
